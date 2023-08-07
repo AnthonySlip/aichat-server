16a2f17a70d71485a92e55f74aca967029ada520
Anton <slipchi@MacBook-Air-Anton.local>
2023-08-07 20:00:50 +0300

    first commit

--- model/user-model.js
+++ model/user-model.js
@@ -4,7 +4,7 @@ const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
-    isVerify: {type: Boolean, default: false},
+    isVerify: {type: Boolean, default: true},
     verificationCode: {type: String},
 })
 
